6ca625e81953164ffebfec77e7d535640afdda5b
wangxueqing <1245831750@qq.com>
2021-03-16 11:35:49 +0800

    tijiao

--- school - 副本/deploy.sh
+++
@@ -1,9 +0,0 @@
-﻿
-
-# 发送
-echo "发送...."
-scp -r ./target/blade-api.jar root@8.136.115.217:/myProject &&
-# 重启App
-echo "重启...."
-ssh root@8.136.115.217 "cd /myProject;./service.sh restart;./service.sh log;"
-
